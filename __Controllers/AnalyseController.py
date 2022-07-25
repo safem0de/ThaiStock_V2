a4e5845d10447b02482dacdae944b1e29ac72807
safem0de <n.wangwanich@gmail.com>
2022-07-25 17:04:01 +0700

    V0.3

--- __Controllers/AnalyseController.py
+++ __Controllers/AnalyseController.py
@@ -1,8 +1,11 @@
-
+from __Models.Stocks import Stock
 
 class AnalyseController():
 
-    def __init__(self) -> None:
+    def __init__(self, model:Stock) -> None:
         super().__init__()
+        self.model = model
 
-    
+    def bind(self, model:Stock, view):
+        self.view = view
+        
