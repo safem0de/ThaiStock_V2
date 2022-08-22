aafc9e8d452652657331ea72e55bdfbe96eb54e1
safem0de <n.wangwanich@gmail.com>
2022-08-22 12:31:14 +0700

    condition handle error on vwap24Hr

--- __Models/Stocks.py
+++ __Models/Stocks.py
@@ -69,7 +69,7 @@ class Stock:
                 '-',
                 round(float(r.get('volumeUsd24Hr')),4),
                 round(float(r.get('marketCapUsd'))/1000000,4),
-                # round(float(r.get('vwap24Hr')),4),
+                round(float(r.get('vwap24Hr')),4) if r.get('vwap24Hr') != None else '-',
                 ]}
             self.__Market['Crypto'].update(y)
 
